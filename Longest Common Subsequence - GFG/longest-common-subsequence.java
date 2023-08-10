db81521074c5039d3be1e99cf3891919cf1fc7bb
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-08-10 16:20:01 +0530

    Added solution - LeetHub

---
+++ Longest Common Subsequence - GFG/longest-common-subsequence.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		int test=sc.nextInt();
+		while(test-- > 0){
+		    int p=sc.nextInt();             // Take size of both the strings as input
+		    int q=sc.nextInt();
+		    
+		    String s1=sc.next();            // Take both the string as input
+	        String s2=sc.next();
+		    
+		    Solution obj = new Solution();
+		    
+		    System.out.println(obj.lcs(p, q, s1, s2));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the length of longest common subsequence in two strings.
+    static int lcs(int x, int y, String text1, String text2)
+    {
+        // your code here
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int i=0;i<=text1.length();i++){
+            for(int j=0;j<=text2.length();j++){
+                if(i==0 || j==0)dp[i][j]=0;
+                else if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    
+}
+}
