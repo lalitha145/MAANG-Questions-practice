3916d53cee778daad66221a2bc50212baaf372ff
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-08-15 16:39:22 +0530

    Time: 12 ms (53.36%), Space: 48.4 MB (37.69%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -1,23 +1,26 @@
 class Solution {
-    int mod = 1000000007;
+    static int mod = 1000000007;
     public int countGoodStrings(int low, int high, int zero, int one) {
-        int ans = 0;
-        int[] dp = new int[high + 1];
-        Arrays.fill(dp, -1);
-        for(int i = low; i <= high; i++){
-            // ans = ((ans) % mod + (helper(i, high, low, zero, one)) % mod) % mod;
-            ans = ((ans) % mod + (helperMem(i, low, high, zero, one, dp)) % mod) % mod;
-        }
-        return ans;
+       int ans=0;
+       int dp[]=new int[high+1];
+       Arrays.fill(dp,-1);
+       for(int i=low;i<=high;i++){
+           ans=ans%mod+helper(i,dp,low,high,zero,one)%mod;
+       }
+       return ans%mod;
     }
-
-
-    public int helperMem(int val, int low, int high, int zero, int one, int[] dp){
-        if(val == 0) return 1;
-        if(val < 0) return 0;
-        if(dp[val] != -1) return dp[val];
-        int first = helperMem(val - zero, low, high, zero, one, dp);
-        int second = helperMem(val - one, low, high, zero, one, dp);
-        return dp[val] = (first + second) % mod;
+    static int helper(int i,int dp[],int l,int h,int z,int o){
+        if(i==0){
+            return 1;
+        }
+        if(i<0){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int first=helper(i-z,dp,l,h,z,o);
+        int second=helper(i-o,dp,l,h,z,o);
+        return dp[i]=(first%mod+second%mod)%mod;
     }
 }
