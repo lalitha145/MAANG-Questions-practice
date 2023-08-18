cf7fb58661e8e5b14e7d65c8338a678316a83392
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-08-18 09:24:05 +0530

    Time: 4 ms (88.27%), Space: 44.3 MB (46.73%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.java
+++ 1738-maximal-network-rank/1738-maximal-network-rank.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        int count=0;
-        for(int i=0;i<n;i++){
-           
-            for(int j=i+1;j<n;j++){
-                 int max=0;
-                
-                for(int a[]:roads){
-                    if(a[0]==i || a[0]==j || a[1]==i || a[1]==j){
-                        max++;
-                    }
-                }
-                count=Math.max(count,max);
+        int rank=0;
+        int count[]=new int[n];
+        int directlyConnected[][]=new int[n][n];
+        for(int road[]:roads){
+            count[road[0]]++;
+            count[road[1]]++;
+            directlyConnected[road[0]][road[1]]=1;
+            directlyConnected[road[1]][road[0]]=1;
 
-            }
-            
+
         }
-        return count;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                rank=Math.max(rank,count[i]+count[j]-directlyConnected[i][j]);
+            }
+        }
+        return rank;
     }
 }
