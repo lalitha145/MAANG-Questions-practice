e6f23688aa44637d07cc49c356295f61385369a7
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-10-06 14:19:42 +0530

    Time: 2310 ms (5.09%), Space: 43.1 MB (96.57%) - LeetHub

--- 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.java
@@ -1,21 +1,19 @@
 class Solution {
     public int longestArithSeqLength(int[] nums) {
-        int maxLength = 2; // Initialize the length of the longest arithmetic subsequence to 2
-        
-        for (int i = 0; i < nums.length - 1; i++) { // Loop through all pairs of elements in the array
+        int maxLength = 2; 
+        for (int i = 0; i < nums.length - 1; i++) { 
             for (int j = i + 1; j < nums.length; j++) {
-                int diff = nums[j] - nums[i]; // Calculate the difference between the two elements
-                int length = 2; // Initialize the length of the current subsequence to 2
-                int current = nums[j]; // Set the current element to the second element of the subsequence
+                int diff = nums[j] - nums[i]; 
+                int length = 2; 
+                int current = nums[j]; 
                 
-                for (int k = j + 1; k < nums.length; k++) { // Loop through the remaining elements of the array
-                    if (nums[k] - current == diff) { // Check if the next element is part of the subsequence
-                        length++; // Increment the length of the subsequence
-                        current = nums[k]; // Set the current element to the next element of the subsequence
+                for (int k = j + 1; k < nums.length; k++) { 
+                    if (nums[k] - current == diff) {
+                        length++; 
+                        current = nums[k]; 
                     }
                 }
-                
-                maxLength = Math.max(maxLength, length); // Update the length of the longest subsequence if the current subsequence is longer
+                maxLength = Math.max(maxLength, length); 
             }
         }       
         return maxLength;
