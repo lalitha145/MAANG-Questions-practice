d8456394e91fd64cd6770a44288f6014d2ad53df
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-10-06 15:34:41 +0530

    Time: 3 ms (64.49%), Space: 43.4 MB (39.49%) - LeetHub

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/2883-partition-string-into-minimum-beautiful-substrings.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    int ans=Integer.MAX_VALUE;
+    public boolean isPower5(String s){
+        long num = 0;
+        for(char c : s.toCharArray()){
+            num = num*2 + (c-'0');
+        }
+        while(num > 1){
+            if(num%5 != 0){
+                return false;
+            }
+            num = num/5;
+        }
+        return num==1;
+    }
+    public void partition(int i, int n, String s, int curr){
+        if(i >= n){
+           ans=Math.min(ans,curr);
+        }
+
+        for(int j = i; j < n; j++){
+            if(s.charAt(i) != '0' && isPower5(s.substring(i, j+1))){
+                partition(j+1, n, s, curr+1);
+            }
+        }
+    }
+    public int minimumBeautifulSubstrings(String s) {
+        partition(0, s.length(), s, 0);
+     return ans==Integer.MAX_VALUE?-1:ans;
+    }
+}
