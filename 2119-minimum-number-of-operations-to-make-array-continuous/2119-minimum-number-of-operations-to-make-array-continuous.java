97101b58c5ae24a92dda2721a25bd47e6fa104cb
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2023-10-12 22:37:31 +0530

    Time: 36 ms (96.15%), Space: 57.8 MB (49.08%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.java
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,16 +1,18 @@
 class Solution {
     public int minOperations(int[] nums) {
         Arrays.sort(nums);
-        int uniqueLen = 1;
+        int m = 1;
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] != nums[i - 1]) {
-                nums[uniqueLen++] = nums[i];
+                nums[m++] = nums[i];
             }
         }
         
         int ans = nums.length;
-        for (int i = 0, j = 0; i < uniqueLen; ++i) {
-            while (j < uniqueLen && nums[j] - nums[i] <= nums.length - 1) {
+        
+        for (int i = 0, j=0; i < m; ++i) {
+           
+            while (j < m && nums[j] - nums[i] <= nums.length - 1) {
                 j++;
             }
             ans = Math.min(ans, nums.length - (j - i));
