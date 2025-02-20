492e7ccd2043508d8da74c845859f9f240f5a8bc
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-02-20 21:07:22 +0530

    Time: 22 ms (29.42%) | Memory: 45.8 MB (9.07%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,22 +1,19 @@
 class Solution {
     public String getHappyString(int n, int k) {
-        Queue<String> q=new LinkedList<>();
-        List<String> ans=new ArrayList<>();
-        q.add("a");
-        q.add("b");
-        q.add("c");
-        while(!q.isEmpty()){
-            String s=q.poll();
-            if(s.length()==n){
-                ans.add(s);
-                continue;
-            }
-            for(char ch:new char[]{'a','b','c'}){
-                if(s.charAt(s.length()-1)!=ch){
-                    q.add(s+ch);
-                }
-            }
+        List<String> l=new LinkedList<>();
+        helper(n,l,"",0);
+       // Collections.sort(l);
+        return l.size()<k?"":l.get(k-1);
+    }
+    void helper(int n,List<String> l,String s,int ind){
+      if(ind==n){
+        l.add(s);
+        return;
+      }
+      for(char c='a';c<='c';c++){
+        if(s.length()==0 ||s.charAt(s.length()-1)!=c){
+            helper(n,l,s+c,ind+1);
         }
-        return ans.size()<k?"":ans.get(k-1);
+      }
     }
 }
