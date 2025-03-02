b6d893afab81d603b7e2792b250f6219ce56e3b3
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-02 09:42:27 +0530

    Time: 5 ms (28.44%) | Memory: 45.9 MB (8.23%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        //PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
 
         for(int a[]:nums1){
             map.put(a[0],map.getOrDefault(a[0],0)+a[1]);
@@ -9,14 +9,12 @@ class Solution {
         for(int a[]:nums2){
             map.put(a[0],map.getOrDefault(a[0],0)+a[1]);
         }
-        for(int id:map.keySet()){
-            pq.add(new int[]{id,map.get(id)});
-        }
-        int ans[][]=new int[pq.size()][2];
+        int ans[][]=new int[map.size()][2];
         int i=0;
-        while(!pq.isEmpty()){
-            ans[i++]=pq.poll();
+        for(int id:map.keySet()){
+             ans[i++]=new int[]{id,map.get(id)};
         }
+        
         return ans;
     }
 }
