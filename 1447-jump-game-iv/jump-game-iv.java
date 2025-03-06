eec68ad0be0dc8fce7671b9d67f4a7b679ca098e
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-06 19:40:23 +0530

    Time: 78 ms (27.96%) | Memory: 63.3 MB (26.88%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -10,37 +10,37 @@ class Solution {
                 map.get(arr[i]).add(i);
             }
         }
-        //Queue gonna store index and steps
+        //Queue gonna store ele and index
         boolean vis[]=new boolean[arr.length];
         q.add(new int[]{0,0});
-      
+        int count=0;
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 int poll[]=q.poll();
-                int ind=poll[0];
-                int steps=poll[1];
-                if(ind==arr.length-1) return steps;
+                int ele=poll[0];
+                int ind=poll[1];
+                if(ind==arr.length-1) return count;
                 if(ind-1>=0 && !vis[ind-1]){
-                    q.add(new int[]{ind-1,steps+1});
+                    q.add(new int[]{arr[ind-1],ind-1});
                     vis[ind-1]=true;
                 }
                 if(ind+1<arr.length && !vis[ind+1]){
-                    q.add(new int[]{ind+1,steps+1});
+                    q.add(new int[]{arr[ind+1],ind+1});
                     vis[ind+1]=true;
 
                 }
-                if(!map.containsKey(arr[ind])) continue;
-                for(int j=0;j<map.get(arr[ind]).size();j++){
+                if(map.containsKey(arr[ind])){
+                    for(int j=0;j<map.get(arr[ind]).size();j++){
                         if(map.get(arr[ind]).get(j)!=ind && !vis[map.get(arr[ind]).get(j)]){
-                            q.add(new int[]{map.get(arr[ind]).get(j),steps+1});
+                            q.add(new int[]{arr[ind],map.get(arr[ind]).get(j)});
                             vis[map.get(arr[ind]).get(j)]=true;
+                        }
                     }
+                    map.remove(arr[ind]);
                 }
-                 map.remove(arr[ind]);
-                
             }
-           
+            count++;
         }
         return -1;
     }
