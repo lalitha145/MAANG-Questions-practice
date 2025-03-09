845f8c001076027b9a3a9cc41125c73c8b65417e
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-09 20:38:18 +0530

    Time: 13 ms (86.49%) | Memory: 41.2 MB (90.59%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -7,8 +7,8 @@ class Solution {
             for(int j = i+1; j < n ; j++){
                 int temp = 2;
                 for(int k = j+1 ; k<n ; k++ ){                   
-                    int x = (points[j][1] - points[i][1]) * (points[k][0] - points[i][0]);
-                    int y = (points[k][1] - points[i][1]) * (points[j][0] - points[i][0]);
+                    int x = (points[j][1] - points[i][1]) * (points[k][0] - points[j][0]);
+                    int y = (points[k][1] - points[j][1]) * (points[j][0] - points[i][0]);
                     if(x == y){
                         temp++;
                     }
