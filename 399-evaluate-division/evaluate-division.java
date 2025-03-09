336a7a2994699715cc59913fdc653a4a70cf720a
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-09 15:33:16 +0530

    Time: 1 ms (99.12%) | Memory: 42.5 MB (22.51%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,56 +1,50 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String,HashMap<String,Double>> map=buildGraph(equations,values);
+        HashMap<String,HashMap<String,Double>> map=buildMap(equations,values);
         double ans[]=new double[queries.size()];
         int ind=0;
         for(List<String> query:queries){
-            String divisor=query.get(0);
-            String dividend=query.get(1);
-            if(!map.containsKey(divisor) ||!map.containsKey(dividend)){
-                ans[ind]=-1.0;
-            }else{
-            HashSet<String> vis=new HashSet<>();
-            double tempAns[]=new double[1];
-            tempAns[0]=-1.0;
-            double temp=1.0;
-            dfs(map,divisor,dividend,tempAns,temp,vis);
-            ans[ind]=tempAns[0];
+            String src=query.get(0);
+            String dest=query.get(1);
+            if(!map.containsKey(src) ||!map.containsKey(dest)) ans[ind++]=-1.0;
+            else{
+              HashSet<String> vis=new HashSet<>();
+              double[] tempAns=new double[1];
+              tempAns[0]=-1.0;
+              double temp=1.0;
+              dfs(vis,map,tempAns,temp,src,dest);
+              ans[ind]=tempAns[0];
+              ind++;
             }
-            ind++;
         }
         return ans;
 
     }
-    void dfs(HashMap<String,HashMap<String,Double>> map,String source,String dest,double ans[],double temp,HashSet<String> vis){
-        vis.add(source);
-        if(source.equals(dest)){
-            ans[0]=temp;
+    void dfs(HashSet<String> vis, HashMap<String,HashMap<String,Double>> map, double tempAns[], double temp, String src, String dest){
+        vis.add(src);
+        if(src.equals(dest)){
+            tempAns[0]=temp;
             return;
         }
-        for(Map.Entry<String,Double> entry:map.get(source).entrySet()){
+        for(Map.Entry<String,Double> entry:map.get(src).entrySet()){
            String nb=entry.getKey();
            double val=entry.getValue();
            if(!vis.contains(nb)){
-            dfs(map,nb,dest,ans,temp*val,vis);
+            dfs(vis,map,tempAns,temp*val,nb,dest);
            }
         }
     }
-    HashMap<String,HashMap<String,Double>> buildGraph(List<List<String>> e, double[] v){
+    HashMap<String,HashMap<String,Double>> buildMap(List<List<String>> equations,double values[]){
         HashMap<String,HashMap<String,Double>> map=new HashMap<>();
         int i=0;
-        for(List<String> pair:e){
-            String divisor=pair.get(0);
-            String dividend=pair.get(1);
-            if(!map.containsKey(divisor)){
-                map.put(divisor,new HashMap<>());
-            }
-            if(!map.containsKey(dividend)){
-                map.put(dividend,new HashMap<>());
-            }
-            map.get(divisor).put(dividend,v[i]);
-            map.get(dividend).put(divisor,1.0/v[i]);
+        for(List<String> list:equations){
+            String divisor=list.get(0);
+            String dividend=list.get(1);
+            map.putIfAbsent(divisor,new HashMap<>());
+            map.putIfAbsent(dividend,new HashMap<>());
+            map.get(divisor).put(dividend,values[i]);
+            map.get(dividend).put(divisor,1.0/values[i]);
             i++;
-
         }
         return map;
     }
