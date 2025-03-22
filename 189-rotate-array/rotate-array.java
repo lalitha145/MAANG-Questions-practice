4120a01606bf8fbcd80010ee02ae72bed0a4a2f6
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-22 10:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (64.87%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,17 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
-        int temp[]=Arrays.copyOf(nums,nums.length);
-        int j=0;
-        for(int i=nums.length-k;i<nums.length;i++){
-            temp[j++]=nums[i];
+        reverse(0,nums.length-1,nums);
+        reverse(0,k-1,nums);
+        reverse(k,nums.length-1,nums);
+    }
+    void reverse(int i,int j,int nums[]){
+        while(i<=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
-        for(int i=0;i<nums.length-k;i++){
-            temp[j++]=nums[i];
-        }
-        System.out.println(Arrays.toString(temp));
-        for(int i=0;i<nums.length;i++){
-            nums[i]=temp[i];
-        }
-        // nums=Arrays.copyOf(temp,temp.length);
     }
 }
