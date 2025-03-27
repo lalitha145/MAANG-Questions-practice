85812f3e7ac1a7391ad68253f909c4a12c8e90d7
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-27 12:33:35 +0530

    Time: 5 ms (6.72%) | Memory: 42.7 MB (38.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,46 +1,45 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-       // if(grid.length==1 && grid[0][0]==0) return 0;
-       boolean isFresh=false;
         Queue<int[]> q=new LinkedList<>();
-        boolean vis[][]=new boolean[grid.length][grid[0].length];
+        int min=-1;
+        boolean f=false;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1) isFresh=true;
+                if(grid[i][j]==1){
+                    f=true;
+                }
                 if(grid[i][j]==2){
-                    vis[i][j]=true;
                     q.add(new int[]{i,j});
                 }
             }
         }
-        int level=0;
-        int dir[][]={{0,1},{1,0},{-1,0},{0,-1}};
-  
-      if(isFresh==false) return 0;
-       if(q.isEmpty()) return -1;
+        if(f==false) return 0;
+        if(q.size()==0) return -1;
+        int dir[][]={{-1,0},{0,-1},{1,0},{0,1}};
         while(!q.isEmpty()){
             int size=q.size();
-            level++;
-            for(int i1=0;i1<size;i1++){
-                int poll[]=q.poll();
-                int row=poll[0];
-                int col=poll[1];
-                for(int d[]:dir){
-                    int i=row+d[0];
-                    int j=col+d[1];
-                    if(i>=0 && j>=0 && i<grid.length && j<grid[0].length && !vis[i][j] && grid[i][j]==1){
-                        vis[i][j]=true;
-                        q.add(new int[]{i,j});
-                    }
+            for(int i=0;i<size;i++){
+            int poll[]=q.poll();
+            int r=poll[0],c=poll[1];
+
+            for(int d[]:dir){
+                int nr=r+d[0], nc=c+d[1];
+                if(nr>=0 && nc>=0 && nr< grid.length && nc<grid[0].length && grid[nr][nc]==1){
+                    q.add(new int[]{nr,nc});
+                    grid[nr][nc]=2;
                 }
             }
+            }
+            min++;
         }
-        
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(vis[i][j]==false && grid[i][j]!=0) return -1;
+
+                // System.out.print(grid[i][j]+"  ");
+                if(grid[i][j]==1) return -1;
             }
+            System.out.println();
         }
-        return level-1;
+        return min;
     }
 }
