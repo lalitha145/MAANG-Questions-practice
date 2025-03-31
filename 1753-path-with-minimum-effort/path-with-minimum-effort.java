bb8b261b6651f7728f505439bdca0c4b5eb5cd09
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-31 14:08:41 +0530

    Time: 39 ms (82.31%) | Memory: 45.4 MB (54.02%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,37 +1,43 @@
+class Pair{
+    int row,col,diff;
+    public Pair(int row,int col,int diff){
+        this.row=row;
+        this.col=col;
+        this.diff=diff;
+    }
+}
+
+
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int low=0;
-        int high=Integer.MAX_VALUE;
-        int m=heights.length,n=heights[0].length;
-       // boolean vis[][]=new boolean[m][n];
-        while(low<=high){
-            int mid=(low+high)/2;
-            boolean vis[][]=new boolean[m][n];
-            
-            if(dfs(mid,0,0,heights,vis)){
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return low;
-    }
-    boolean dfs(int mid,int r,int c,int heights[][],boolean vis[][]){
-        vis[r][c]=true;
+        int m=heights.length;
+        int n=heights[0].length;
+        int dis[][]=new int[m][n];
+        for(int a[]:dis) Arrays.fill(a,Integer.MAX_VALUE);
         int dir[][]={{-1,0},{0,-1},{1,0},{0,1}};
-        if(r==heights.length-1 && c==heights[0].length-1) return true;
-        for(int d[]:dir){
-            int nr=d[0]+r;
-            int nc=d[1]+c;
-            if(nr>=0 && nc>=0 && nr<heights.length && nc<heights[0].length && !vis[nr][nc]){
-                int newEffort=Math.abs(heights[r][c]-heights[nr][nc]);
-                if(newEffort<=mid && dfs(mid,nr,nc,heights,vis)){
-                
-                return true;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.diff-b.diff);
+        pq.add(new Pair(0,0,0));
+        while(!pq.isEmpty()){
+            Pair poll=pq.poll();
+            int r=poll.row;
+            int c=poll.col;
+            int d=poll.diff;
+            if(r==m-1 && c==n-1) return d;
+            for(int d1[]:dir){
+                int nr=d1[0]+r;
+                int nc=d1[1]+c;
+                if(nr>=0 && nc>=0 && nr<m && nc<n){
+                  int newDiff=Math.abs(heights[r][c]-heights[nr][nc]);
+                  int effort=Math.max(newDiff,d);
+                  if(dis[nr][nc]>effort){
+                    dis[nr][nc]=newDiff;
+                    pq.add(new Pair(nr,nc,effort));
+                  }
+                }
+
             }
         }
+        return -1;
         
-    }
-    return false;
     }
 }
