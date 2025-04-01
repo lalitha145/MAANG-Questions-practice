d4fc676cfea6ca03770a83117d9709ae7bd58db1
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-01 21:22:17 +0530

    Time: 28 ms (87.21%) | Memory: 47.8 MB (65.97%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,46 +1,80 @@
-import java.util.*;
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+}
+
 
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        HashMap<String, HashSet<String>> map = new HashMap<>();
-        HashMap<String, String> emailToName = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
+    public List<List<String>> accountsMerge(List<List<String>> details) {
 
-        for (List<String> account : accounts) {
-            String name = account.get(0);
-            for (int i = 1; i < account.size(); i++) {
-                String email = account.get(i);
-                emailToName.put(email, name);
-                map.putIfAbsent(email, new HashSet<>());
+        int n = details.size();
+        DisjointSet ds = new DisjointSet(n);
+        HashMap<String, Integer> mapMailNode = new HashMap<String, Integer>();
 
-                if (i > 1) {
-                    map.get(email).add(account.get(i - 1));
-                    map.get(account.get(i - 1)).add(email);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < details.get(i).size(); j++) {
+                String mail = details.get(i).get(j);
+                if (mapMailNode.containsKey(mail) == false) {
+                    mapMailNode.put(mail, i);
+                } else {
+                    ds.unionByRank(i, mapMailNode.get(mail));
                 }
             }
         }
 
-        Set<String> visited = new HashSet<>();
-        for (String email : emailToName.keySet()) {
-            if (!visited.contains(email)) {
-                List<String> mergedAccount = new ArrayList<>();
-                dfs(email, map, visited, mergedAccount);
-                Collections.sort(mergedAccount);
-                mergedAccount.add(0, emailToName.get(email)); 
-                ans.add(mergedAccount);
-            }
+        ArrayList<String>[] mergedMail = new ArrayList[n];
+        for (int i = 0; i < n; i++) mergedMail[i] = new ArrayList<String>();
+        for (Map.Entry<String, Integer> it : mapMailNode.entrySet()) {
+            String mail = it.getKey();
+            int node = ds.findUPar(it.getValue());
+            mergedMail[node].add(mail);
         }
 
+        List<List<String>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0) continue;
+            Collections.sort(mergedMail[i]);
+            List<String> temp = new ArrayList<>();
+            temp.add(details.get(i).get(0));
+            for (String it : mergedMail[i]) {
+                temp.add(it);
+            }
+            ans.add(temp);
+        }
         return ans;
-    }
 
-    private void dfs(String email, HashMap<String, HashSet<String>> map, Set<String> visited, List<String> mergedAccount) {
-        visited.add(email);
-        mergedAccount.add(email);
-        for (String neighbor : map.get(email)) {
-            if (!visited.contains(neighbor)) {
-                dfs(neighbor, map, visited, mergedAccount);
-            }
-        }
     }
 }
