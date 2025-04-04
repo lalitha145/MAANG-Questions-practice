0384b9095929d30f089bddb27ce4d5a04c8bdbfe
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-04 19:34:35 +0530

    Time: 1 ms (99.85%) | Memory: 47.8 MB (44.81%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,43 +1,19 @@
-class Pair{
-    int row, col, value;
-    public Pair(int row,int col, int value){
-        this.row=row;
-        this.col=col;
-        this.value=value;
-    }
-}
 class Solution {
     public int minPathSum(int[][] grid) {
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.value-b.value);
-        pq.add(new Pair(0,0,grid[0][0]));
-        int m=grid.length, n=grid[0].length;
-        boolean vis[][]=new boolean[grid.length][grid[0].length];
-        vis[0][0]=true;
-        while(!pq.isEmpty()){
-            Pair poll=pq.poll();
-            int row=poll.row;
-            int col=poll.col;
-            int value=poll.value;
 
-            if(row==m-1 && col==n-1) return value;
-           
-            if(row+1<m){
-                if(!vis[row+1][col]){
-                  pq.add(new Pair(row+1,col,value+grid[row+1][col]));
-                  vis[row+1][col]=true;
-                }
-
-            }
-           if(col+1<n){
-            if(!vis[row][col+1]){
-                pq.add(new Pair(row,col+1,value+grid[row][col+1]));
-                vis[row][col+1]=true;
-            }
-
-            }
-           
-
-        }
-        return -1;
+        int m=grid.length-1;
+        int n=grid[0].length-1;
+        int dp[][]=new int[m+1][n+1];
+        for(int a[]:dp) Arrays.fill(a,-1);
+        return helper(grid,m,n,dp);
+    }
+    int helper(int grid[][],int m, int n,int dp[][]){
+        if(m==0 && n==0) return grid[0][0];
+        if(m<0 || n<0) return Integer.MAX_VALUE;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int left=helper(grid,m,n-1,dp);
+        int up=helper(grid,m-1,n,dp);
+        int value=grid[m][n]+Math.min(left,up);
+        return dp[m][n]=value;
     }
 }
