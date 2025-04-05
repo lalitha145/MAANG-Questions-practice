c551c02f684e4106955c0a45d55b9bdb5c1ba87c
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-05 09:09:09 +0530

    Time: 41 ms (57.60%) | Memory: 46.5 MB (65.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -6,25 +6,21 @@ class Solution {
         }
         if(sum%2!=0) return false;
         int target=sum/2;
-          int n = nums.length;
-        boolean[][] dp = new boolean[n + 1][target + 1];
-
-        // Initialize: sum 0 is always possible (with empty set)
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+        boolean dp[][]=new boolean[nums.length][target+1];
+        for(int i=0;i<nums.length;i++){
+            dp[i][0]=true;
         }
-
-        // Fill the subset sum table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j]; // pick or not pick
-                } else {
-                    dp[i][j] = dp[i - 1][j]; // can't pick
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<=target;j++){
+                boolean np=dp[i-1][j];
+                boolean pick=false;
+                if(nums[i]<=j){
+                    pick=dp[i-1][j-nums[i]];
                 }
+                dp[i][j]=pick || np;
             }
         }
-
-        return dp[n][target];
+        return dp[nums.length-1][target];
     }
+   
 }
