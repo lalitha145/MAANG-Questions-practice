2c8274fc2b89b969a20c5fadd3df2001b73c0050
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-05 13:52:37 +0530

    Time: 18 ms (42.93%) | Memory: 45.5 MB (17.83%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,18 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[][]=new int[coins.length][amount+1];
-        for(int a[]:dp){
-            Arrays.fill(a,-1);
-        }
-        int ans=helper(coins,amount,coins.length-1,dp);
-        return ans>=(int)1e9?-1:ans;
+         int dp[][]=new int[coins.length][amount+1];
+         for(int i=0;i<coins.length;i++){
+              Arrays.fill(dp[i],(int)1e9);
+         }
+         for(int i=0;i<=amount;i++){
+             if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+             }
+         }
+
+            for(int i=1;i<coins.length;i++){
+                for(int amt=0;amt<=amount;amt++){
+                 
+                    int pick=(int)1e9;
+                    if(amt>=coins[i]){
+                      pick=1+dp[i][amt-coins[i]];
+                    }
+                    int np=dp[i-1][amt];
+                    dp[i][amt]=Math.min(pick,np);
+                }
+            }
+        int ans = dp[coins.length - 1][amount];
+        return (ans >= (int)1e9) ? -1 : ans;
     }
-    int helper(int coins[], int amt, int ind,int dp[][]){
-        if(ind<0|| amt<0) return (int)1e9;
-        if(amt==0) return 0;
-        if(dp[ind][amt]!=-1) return dp[ind][amt];
-        int pick=1+helper(coins,amt-coins[ind],ind,dp);
-        int np=0+helper(coins,amt,ind-1,dp);
-        return dp[ind][amt]=Math.min(pick,np);
-    }
+   
 }
