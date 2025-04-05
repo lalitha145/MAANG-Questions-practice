672f4d4763d5c82682a252024825a81f62d95201
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-05 19:22:52 +0530

    Time: 19 ms (98.29%) | Memory: 55.4 MB (43.69%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -12,7 +12,7 @@ class Solution {
             return 1;
         if(dp[start][end]!=0) return dp[start][end];
         if(s.charAt(start) == s.charAt(end))
-            return 2 + helper(s, start+1, end-1,dp);
+            return dp[start][end]=2 + helper(s, start+1, end-1,dp);
         
         else
              return dp[start][end]=Math.max(helper(s, start+1, end,dp), helper(s, start, end-1,dp));
