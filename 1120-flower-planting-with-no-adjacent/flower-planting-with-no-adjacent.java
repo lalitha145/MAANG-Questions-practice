337c3292a0e35a95d13546b22901ada5f47e8b5a
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-04-06 19:18:21 +0530

    Time: 15 ms (59.11%) | Memory: 52.8 MB (64.72%) - LeetSync

---
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] gardenNoAdj(int n, int[][] paths) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] type = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] path : paths) {
+            int u = path[0] - 1;
+            int v = path[1] - 1;
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        helper(0, adj, n, type);
+        return type;
+    }
+
+    boolean helper(int node, List<List<Integer>> adj, int n, int[] color) {
+        if (node == n) return true;
+
+        for (int i = 1; i <= 4; i++) {
+            if (isSafe(i, adj, color, node)) {
+                color[node] = i;
+                if (helper(node + 1, adj, n, color)) return true;
+                color[node] = 0;
+            }
+        }
+        return false;
+    }
+
+    boolean isSafe(int c, List<List<Integer>> adj, int[] color, int node) {
+        for (int nb : adj.get(node)) {
+            if (color[nb] == c) return false;
+        }
+        return true;
+    }
+}
