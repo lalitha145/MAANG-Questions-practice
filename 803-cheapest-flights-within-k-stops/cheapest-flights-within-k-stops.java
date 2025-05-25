3b76548ec59a699ca4e41c3655ba008c44ead815
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-05-25 22:48:06 +0530

    Time: 6 ms (83.51%) | Memory: 44.6 MB (45.98%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,56 +1,57 @@
 class Pair{
-    int node,cost;
-    public Pair(int node,int cost){
-        this.node=node;
+    int adjNode;
+    int cost;
+    public Pair(int adjNode, int cost){
+        this.adjNode=adjNode;
         this.cost=cost;
     }
+
 }
-class Tuple{
-    int node, cost, K;
-    public Tuple(int node,int cost,int K){
-        this.node=node;
-        this.cost=cost;
-        this.K=K;
+class Tuple {
+    int node, cost, stops;
+    public Tuple(int node, int cost, int stops) {
+        this.node = node;
+        this.cost = cost;
+        this.stops = stops;
     }
 }
- class Solution {
+class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<List<Pair>> adj=new ArrayList<>();
-       Queue<Tuple> pq=new LinkedList<>();
-        int dist[]=new int[n];
-        
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src]=0;
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         for(int f[]:flights){
-            int u=f[0],v=f[1], w=f[2];
-            adj.get(u).add(new Pair(v,w));
+            adj.get(f[0]).add(new Pair(f[1], f[2]));
         }
-        // node. cost, k
+        int dist[]=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        Queue<Tuple> pq=new LinkedList<>();
         pq.add(new Tuple(src,0,0));
+        int ans=Integer.MAX_VALUE;
         while(!pq.isEmpty()){
             Tuple t=pq.poll();
-            int currNode=t.node;
-            int currCost=t.cost;
-            int currK=t.K;
-            // if(currNode==dst){
-            //     return currCost;
-            // }
-            if(currK>k) continue;
-            for(Pair p:adj.get(currNode)){
-                int adjNode=p.node;
-                int adjCost=p.cost;
-                if(adjCost+currCost<dist[adjNode] && currK<=k){
-                    dist[adjNode]=adjCost+currCost;
-                    pq.add(new Tuple(adjNode,dist[adjNode],currK+1));
-                }
+            int node=t.node;
+            int cost=t.cost;
+            int stops=t.stops;
+            if(node==dst){
+                ans=Math.min(ans,cost);
             }
+            if(stops>k) continue;
+            
+            for(Pair p:adj.get(node)){
+                int adjNode=p.adjNode;
+                int adjCost=p.cost;
+                if(dist[adjNode]>adjCost+cost && stops<=k){
+                    dist[adjNode]=adjCost+cost;
+                    pq.add(new Tuple(adjNode, dist[adjNode], stops+1));
+                }
+               
+            }
+           
         }
-       return dist[dst]==Integer.MAX_VALUE? -1: dist[dst];
+        return dist[dst]==Integer.MAX_VALUE?-1:ans;
 
-
-      
     }
 }
