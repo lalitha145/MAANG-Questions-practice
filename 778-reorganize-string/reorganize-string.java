a0e093bc64472b440ecab5e5233cdde4d058f03f
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-06-02 14:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (98.58%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,38 +1,39 @@
 class Solution {
     public String reorganizeString(String s) {
-    int fre[]=new int[26];
-    char res[]=new char[s.length()];
-    int maxFre=0, ind=0;
-    for(int i=0;i<s.length();i++){
-        char c=s.charAt(i);
+     int fre[]=new int[26];
+     char ans[]=new char[s.length()];
+     for(char c:s.toCharArray()){
         fre[c-'a']++;
-    }
-     for(int i=0;i< fre.length;i++){
-        if(fre[i]>maxFre){
-            maxFre=fre[i];
-            ind=i;
-        }
      }
-     int resInd=0;
-     if(maxFre> (s.length()+1)/2) return "";
-     while(fre[ind]>0){
-        res[resInd]=(char)(ind+'a');
-        fre[ind]--;
-        resInd+=2;
-     }
+     int maxFre=0, maxFreIndex=0;
      for(int i=0;i<26;i++){
-        while(fre[i]>0){
-            if(resInd>=s.length()){
-                resInd=1;
-            }
-            res[resInd]=(char)(i+'a');
-            fre[i]--;
-            resInd+=2;
+        if(maxFre<fre[i]){
+            maxFre=fre[i];
+            maxFreIndex=i;
         }
      }
-     return new String(res);
-
+     int i=0;
+     if(maxFre>((s.length()+1)/2)) return "";
+     while(fre[maxFreIndex]>0){
+        ans[i]=(char)(maxFreIndex+'a');
+        
+        fre[maxFreIndex]--;
+        i+=2;
+     }
     
-     
+     for(int j=0;j<26;j++){
+        if(fre[j]>0){
+       while(fre[j]>0){
+        if(i>=s.length()){
+            i=1;
+        }
+        fre[j]--;
+        ans[i]=(char)(j+'a');
+        i+=2;
+       }
+        }
+     }
+     return new String(ans);
+
     }
 }
