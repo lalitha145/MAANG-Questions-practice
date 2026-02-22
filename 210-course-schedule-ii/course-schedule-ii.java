305760960f774c37a2630cb0fb4375e408c5ec06
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-02-23 00:27:19 +0530

    Time: 6 ms (49.64%) | Memory: 46.8 MB (69.42%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
         int ans[]=new int[numCourses];
-        Stack<Integer> stack=new Stack<>();
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<prerequisites.length;i++){
-            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+
+        int[] indegree = new int[numCourses];
+
+        for (int[] p : prerequisites) {
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
         }
-        boolean vis[]=new boolean[numCourses];
-        boolean path[]=new boolean[numCourses];
-        for(int i=0;i<vis.length;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,adj,stack,path)) return new int[]{};
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
             }
         }
-        int i=0;
-        while(!stack.isEmpty()){
-            ans[i++]=stack.pop();
-        }
-        return ans;
 
-    }
-    boolean dfs(int node,boolean vis[],List<List<Integer>> adj,Stack<Integer> stack,boolean path[]){
-        vis[node]=true;
-        path[node]=true;
-        for(int nb:adj.get(node)){
-            if(!vis[nb]){
-                if(dfs(nb,vis,adj,stack,path))return true;
-                
-            }else if(path[nb]==true) return true;
+        int completed = 0;
+        int i=0;
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            completed++;
+            ans[i++]=course;
+
+            for (int next : adj.get(course)) {
+                if (--indegree[next] == 0) {
+                    q.add(next);
+                }
+            }
         }
-        stack.push(node);
-        path[node]=false;
-        return false;
+        if(completed!=numCourses) return new int[]{};
+
+        return ans;
+ 
     }
 }
