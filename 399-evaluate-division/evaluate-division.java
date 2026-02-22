e473c98e93cf60aa7b3d67c1c04e66d9868d8ff8
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-02-22 22:04:19 +0530

    Time: 2 ms (39.86%) | Memory: 47.1 MB (48.46%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,51 +1,62 @@
-class Solution {
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String,HashMap<String,Double>> map=buildMap(equations,values);
-        double ans[]=new double[queries.size()];
-        int ind=0;
-        for(List<String> query:queries){
-            String src=query.get(0);
-            String dest=query.get(1);
-            if(!map.containsKey(src) ||!map.containsKey(dest)) ans[ind++]=-1.0;
-            else{
-              HashSet<String> vis=new HashSet<>();
-              double[] tempAns=new double[1];
-              tempAns[0]=-1.0;
-              double temp=1.0;
-              dfs(vis,map,tempAns,temp,src,dest);
-              ans[ind]=tempAns[0];
-              ind++;
-            }
-        }
-        return ans;
-
-    }
-    void dfs(HashSet<String> vis, HashMap<String,HashMap<String,Double>> map, double tempAns[], double temp, String src, String dest){
-        vis.add(src);
-        if(src.equals(dest)){
-            tempAns[0]=temp;
-            return;
-        }
-        for(Map.Entry<String,Double> entry:map.get(src).entrySet()){
-           String nb=entry.getKey();
-           double val=entry.getValue();
-           if(!vis.contains(nb)){
-            dfs(vis,map,tempAns,temp*val,nb,dest);
-           }
-        }
-    }
-    HashMap<String,HashMap<String,Double>> buildMap(List<List<String>> equations,double values[]){
-        HashMap<String,HashMap<String,Double>> map=new HashMap<>();
-        int i=0;
-        for(List<String> list:equations){
-            String divisor=list.get(0);
-            String dividend=list.get(1);
-            map.putIfAbsent(divisor,new HashMap<>());
-            map.putIfAbsent(dividend,new HashMap<>());
-            map.get(divisor).put(dividend,values[i]);
-            map.get(dividend).put(divisor,1.0/values[i]);
-            i++;
-        }
-        return map;
+class Pair{
+    String node;
+    double val;
+    public Pair(String node, double val){
+        this.node=node;
+        this.val=val;
     }
 }
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        HashMap<String,List<Pair>> adj=new HashMap<>();
+        double ans[]=new double[queries.size()];
+        for(int i=0;i<equations.size();i++){
+          String from=equations.get(i).get(0);
+          String to=equations.get(i).get(1);
+          double val=values[i];
+          adj.computeIfAbsent(from, k->new ArrayList<>()).add(new Pair(to,val));
+          adj.computeIfAbsent(to, k->new ArrayList<>()).add(new Pair(from,1.0/val));
+        }
+        int i=0;
+        for(List<String> query:queries){
+            String from=query.get(0);
+            String to=query.get(1);
+            double temp[]=new double[]{1.0};
+            if(!adj.containsKey(from) || !adj.containsKey(to)) {
+            ans[i]=-1.0;
+            }
+            else {
+                HashSet<String> vis=new HashSet<>();
+                boolean found=dfs(adj, query.get(0), query.get(1), temp, vis);
+
+                ans[i]=found?temp[0]:-1.0;
+            }
+            i++;
+        }
+        return ans;
+    }
+    boolean dfs(HashMap<String,List<Pair>> adj,
+            String from, String to,
+            double[] temp,
+            HashSet<String> vis) {
+
+    if (from.equals(to)) return true;
+
+    vis.add(from);
+
+    for (Pair p : adj.get(from)) {
+        if (!vis.contains(p.node)) {
+            double prev = temp[0];
+            temp[0] *= p.val;
+
+            if (dfs(adj, p.node, to, temp, vis)) {
+                return true; 
+            }
+
+            temp[0] = prev; 
+        }
+    }
+    return false;
+}
+}
+
