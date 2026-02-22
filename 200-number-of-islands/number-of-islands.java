2747284e1de2ed0a342269cd780646bef6e7157e
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-02-22 20:41:45 +0530

    Time: 6 ms (23.01%) | Memory: 51.1 MB (98.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,7 +8,8 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
                      cnt++;
-                     dfs(vis, grid, i,j);
+                    // dfs(vis, grid, i,j);
+                     bfs(vis, grid, i, j);
                 }
             }
         }
@@ -24,6 +25,27 @@ class Solution {
             int nr=d[0]+i;
             int nc=d[1]+j;
             dfs(vis,grid, nr,nc);
+        }
+
+    }
+    void bfs(boolean vis[][],char grid[][], int i, int j){
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{i,j});
+        int dir[][]={{-1,0}, {0,-1}, {1,0},{0,1}};
+        vis[i][j]=true;
+        while(!q.isEmpty()){
+            int []poll=q.poll();
+            int r=poll[0];
+            int c=poll[1];
+            for(int d[]:dir){
+                int nr=d[0]+r;
+                int nc=d[1]+c;
+                if(nr>=grid.length || nc>=grid[0].length ||nr<0 || nc<0 || vis[nr][nc] || grid[nr][nc]=='0') continue;
+                q.add(new int[]{nr,nc});
+                vis[nr][nc]=true;
+
+
+            }
         }
 
     }
