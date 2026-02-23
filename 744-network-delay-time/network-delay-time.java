88ed409f39d78f808528d26a42dbee30adb935f0
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-02-23 22:09:02 +0530

    Time: 13 ms (50.93%) | Memory: 50.2 MB (24.24%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,42 +1,39 @@
-class Pair{
-    int node, w;
-    public Pair(int node, int w){
-        this.node=node;
-        this.w=w;
-    }
-}
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int dist[]=new int[n+1];
-        int ans=Integer.MIN_VALUE;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.w-b.w);
-        pq.add(new Pair(k,0));
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
-        List<List<Pair>> adj=new ArrayList<>();
-        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
-        for(int t[]:times){
-            int u=t[0], v=t[1], w=t[2];
-            adj.get(u).add(new Pair(v,w));
-        }
-        while(!pq.isEmpty()){
-           Pair poll=pq.poll();
-           int node=poll.node;
-           int w=poll.w;
-           for(Pair p:adj.get(node)){
-            int adjNode=p.node;
-            int adjW=p.w;
-            if(dist[adjNode]>adjW+w){
-                dist[adjNode]=adjW+w;
-                pq.add(new Pair(adjNode, dist[adjNode]));
-            }
-           }
-        }
-        for(int i=1;i<=n;i++){
-            if(dist[i]==Integer.MAX_VALUE) return -1;
-            ans=Math.max(ans,dist[i]);
-        }
-        return ans;
-    }
+       HashMap<Integer, List<int[]>> adj=new HashMap<>();
+       for(int time[]:times){
+            adj.computeIfAbsent(time[0],k1->new ArrayList<>()).add(new int[]{time[1], time[2]});
+       }
+       Queue<int[]> q=new LinkedList<>();
+       q.add(new int[]{k,0});
+       int dis[]=new int[n+1];
+    
+       Arrays.fill(dis, Integer.MAX_VALUE);
+               int cnt=0;
+        dis[k]=0;
 
+       while(!q.isEmpty()){
+        int  poll[]=q.poll();
+        int node=poll[0];
+        int disSoFar=poll[1];
+        if(adj.get(node)==null) continue;
+        for(int a[]:adj.get(node)){
+            int adjDis=a[1];
+            int adjNode=a[0];
+            if(dis[adjNode]> adjDis+disSoFar){
+                dis[adjNode]=adjDis+disSoFar;
+                q.add(new int[]{adjNode, adjDis+disSoFar});
+
+                cnt++;
+            }
+        }
+
+       }
+       int max=Integer.MIN_VALUE;
+        for(int i=1;i<dis.length;i++){
+            max=Math.max(max, dis[i]);
+        }
+       return max==Integer.MAX_VALUE?-1: max;
+       
+    }
 }
