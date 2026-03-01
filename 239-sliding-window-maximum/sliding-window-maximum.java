245b509a1ef8f76765190db3bb38ba8c3f04d28e
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-03-01 14:47:51 +0530

    Time: 34 ms (26.59%) | Memory: 152.5 MB (28.98%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -5,16 +5,10 @@ class Solution {
         Deque<Integer> q=new LinkedList<>();
         int j=0;
         for(int i=0;i<n;i++){
-            if(!q.isEmpty() && q.peek()==i-k){
-                q.poll();
-            }
-            while(!q.isEmpty() && nums[q.peekLast()]<nums[i]){
-                q.pollLast();
-            }
+            if(!q.isEmpty() && q.peek()==i-k) q.pollFirst();
+            while(!q.isEmpty() && nums[q.peekLast()]<=nums[i]) q.pollLast();
             q.addLast(i);
-            if(i>=k-1){
-                ans[j++]=nums[q.peek()];
-            }
+            if(i>=k-1) ans[j++]=nums[q.peek()];
         }
         return ans;
     }
