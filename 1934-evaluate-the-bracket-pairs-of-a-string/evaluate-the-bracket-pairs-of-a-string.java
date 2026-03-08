be03f625fe730b60f1ef2a620e29049153a43915
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2026-03-08 21:53:23 +0530

    Time: 149 ms (7.34%) | Memory: 101.5 MB (5.24%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,30 +1,33 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         HashMap<String,String> map=new HashMap<>();
-        Stack<Character> stack=new Stack<>();
-        for(List<String> l:knowledge){
-           map.put(l.get(0),l.get(1));
-        }
+        int i=0;
         String ans="";
-        for(int i=0;i<s.length();){
-           
-           char c=s.charAt(i);
-           if(c=='('){
-            String temp="";
-            i++;
-             while(i<s.length() && s.charAt(i)!=')'){
-                temp+=s.charAt(i);
+        for(List<String> l:knowledge){
+            map.put(l.get(0),l.get(1));
+        }
+        while(i<s.length()){
+
+            if(s.charAt(i)=='('){
+                StringBuilder sb=new StringBuilder();
+                int j=i+1;
+                while(j<s.length() && s.charAt(j)!=')'){
+                    sb.append(s.charAt(j++));
+                }
+                if(!map.containsKey(sb.toString())){
+                    ans+='?';
+                }else{
+                ans+=map.get(sb.toString());
+                }
+                i=j+1;
+            }else{
+
+                ans+=s.charAt(i);
                 i++;
-             }
-             i++;
-            //  System.out.println()
-             ans+=map.get(temp)==null?"?":map.get(temp);
-           }else{
-            ans+=c;
-            i++;
-           }
+               
+            }
+          
         }
         return ans;
-
     }
 }
